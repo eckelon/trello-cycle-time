5168a9043ebf0b91448b405f888d5ca771cdf6bb
Néstor Salceda <nestor.salceda@gmail.com>
2017-02-12 21:25:09 +0100

    Added support for multiple done and fix several time calculations checking only idList changes

--- trello_card_collection.rb
+++ trello_card_collection.rb
@@ -13,20 +13,25 @@ class TrelloCardCollection
     board = boards.select{ |board| board.name == board_name }[0]
     return [] if board.nil?
 
-    closed_list = board.lists.target.select{ |list| list.name == 'Done' }[0]
-    return [] if closed_list.nil?
+    closed_lists = board.lists.target.select{ |list| list.name.start_with?('Done') }
+    return [] if closed_lists.nil?
 
-    closed_list.cards.target.map do |card|
-      actions = card.actions.target
-      Card.new(
-        id: card.id,
-        short_id: card.short_id,
-        name: card.name,
-        url: card.url,
-        started_on: calculate_started_on_from(card, actions),
-        finished_on: calculate_finished_on_from(actions)
-      )
+    result = []
+    closed_lists.each do |closed_list|
+      cards = closed_list.cards.target.map do |card|
+        actions = card.actions(filter: 'updateCard:idList,createCard').target
+        Card.new(
+          id: card.id,
+          short_id: card.short_id,
+          name: card.name,
+          url: card.url,
+          started_on: calculate_started_on_from(card, actions),
+          finished_on: calculate_finished_on_from(actions)
+        )
+      end
+      result.concat(cards)
     end
+    result
   end
 
   private
@@ -34,13 +39,6 @@ class TrelloCardCollection
   def calculate_started_on_from(card, actions)
     started_on = nil
     actions.each do |action|
-      if action.data.include? 'list'
-        if action.data['list']['name'].start_with? 'Doing'
-          if started_on.nil? or started_on > action.date
-            started_on = action.date
-          end
-        end
-      end
       if action.data.include? 'listAfter'
         if action.data['listAfter']['name'].start_with? 'Doing'
           if started_on.nil? or started_on > action.date
@@ -50,6 +48,15 @@ class TrelloCardCollection
       end
     end
 
+    actions.each do |action|
+      if action.data.include? 'list'
+        if not action.data['list']['name'].nil? and action.data['list']['name'].start_with? 'Doing'
+          if started_on.nil? or started_on > action.date
+            started_on = action.date
+          end
+        end
+      end
+    end
     return card.created_at if started_on.nil?
     started_on
   end
@@ -57,11 +64,21 @@ class TrelloCardCollection
   def calculate_finished_on_from(actions)
     finished_on = nil
     actions.each do |action|
-      next unless action.data.include? 'listAfter'
+      if action.data.include? 'listAfter'
+        if action.data['listAfter']['name'].start_with? 'Done'
+          if finished_on.nil? or finished_on < action.date
+            finished_on = action.date
+          end
+        end
+      end
+    end
 
-      if action.data['listAfter']['name'].start_with? 'Done'
-        if finished_on.nil? or finished_on < action.date
-          finished_on = action.date
+    actions.each do |action|
+      if action.data.include? 'list'
+        if action.data['list']['name'].start_with? 'Done'
+          if finished_on.nil? or finished_on < action.date
+            finished_on = action.date
+          end
         end
       end
     end
