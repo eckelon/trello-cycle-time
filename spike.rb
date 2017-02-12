7bd8d9c615e8b7e9e4524d6bd9cf6d455e4610c4
Néstor Salceda <nestor.salceda@gmail.com>
2017-02-12 19:51:34 +0100

    Generate a report file for each invocation

--- spike.rb
+++ spike.rb
@@ -3,9 +3,12 @@ require 'erb'
 
 card_collection = TrelloCardCollection.new
 board_name = 'Conecta - Tareas'
+now = Time.now
 cards = card_collection.finished_cards_for(board_name)
 
 average_cycle_time = cards.inject(0.0) { |sum, el| sum + el.cycle_time } / cards.length
 
 erb = ERB.new(File.read('report.erb'))
-puts erb.result(binding)
+result = erb.result(binding)
+
+File.write("#{board_name.gsub(' ', '').downcase}_#{now.strftime("%Y%m%d%H%M")}.html", result)
