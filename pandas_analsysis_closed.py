13baa40c6bd1cb81c0dadee5a2eed7b050b1a01a
Néstor Salceda <nestor.salceda@gmail.com>
2017-03-27 09:28:37 +0200

    Add a Jinja2 report and a preview of raw data

--- pandas_analsysis_closed.py
+++ pandas_analsysis_closed.py
@@ -1,44 +1,59 @@
+import os
 from datetime import datetime
-from collections import namedtuple
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import jinja2
 
 class Result(object):
 
-    def __init__(self, cycle_time, throuhgput):
+    def __init__(self, period, cycle_time, throughput):
+        self.period = period
         self._cycle_time = cycle_time
         self.throughput = throughput
 
     @property
     def cycle_time(self):
-        self._cycle_time = cycle_time / 3600
+        return self._cycle_time / 3600
+
+    def __str__(self):
+        return 'cycle_time: {0} throughput: {1}'.format(self.cycle_time, self.throughput)
+
+
+def render(template_path, **context):
+    path, filename = os.path.split(template_path)
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path or './')
+    ).get_template(filename).render(context)
 
 
 filename = 'conecta-tareas-closed-tasks.csv'
-print "Data for: %s" % filename
 
-dataset = pd.read_csv(filename, parse_dates=True, infer_datetime_format=True)#, index_col=0, parse_dates=True, infer_datetime_format=True)
+dataset = pd.read_csv(filename, parse_dates=True, infer_datetime_format=True)
 
 grouped = dataset.groupby('finished_on_week')
 weeks = [name for name, groups in dataset.groupby('finished_on_week')]
 
+results = [
+    Result(
+        weeks[-1],
+        grouped.get_group(weeks[-1])['cycle_time'].mean(),
+        grouped.get_group(weeks[-1])['id'].count()
+    ),
+    Result(
+        " | ".join([weeks[-4], weeks[-3], weeks[-2]]),
+        pd.concat([grouped.get_group(weeks[-4]), grouped.get_group(weeks[-3]), grouped.get_group(weeks[-2])])['cycle_time'].mean(),
+        pd.concat([grouped.get_group(weeks[-4]).count(), grouped.get_group(weeks[-3]).count(), grouped.get_group(weeks[-2]).count()]).mean()
+    ),
+    Result(
+        "total",
+        dataset['cycle_time'].mean(),
+        dataset['id'].count() / len(weeks)
+    )
+]
 
-cycle_times = {
-    'last_week': grouped.get_group(weeks[-1])['cycle_time'].mean() / 3600,
-    'latest_three_weeks': pd.concat([grouped.get_group(weeks[-4]), grouped.get_group(weeks[-3]), grouped.get_group(weeks[-2])])['cycle_time'].mean() / 3600,
-    'total':  dataset['cycle_time'].mean() / 3600
-}
-print cycle_times
-
-throughputs = {
-    'last_week': grouped.get_group(weeks[-1])['id'].count(),
-    'latest_three_weeks': pd.concat([grouped.get_group(weeks[-4]).count(), grouped.get_group(weeks[-3]).count(), grouped.get_group(weeks[-2]).count()]).mean(),
-    'total':  dataset['id'].count() / len(weeks)
-}
-
-print throughputs
-
+r = render('report.j2', board_name='Conecta - Tareas', results=results, now=datetime.now())
+print r
 
 #print dataset.describe()
 
